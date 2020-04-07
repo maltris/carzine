f4de5c270591b09e4627dd62c939a2b6254b7d7d
maltris <m@maltris.org>
2020-04-07 12:12:47 +0200

    Adding autotagebuch.net credits.

--- footer.php
+++ footer.php
@@ -61,7 +61,7 @@
                 <?php else: ?>
                     <p>
                     <a class="text-center" href="<?php echo esc_url( __( 'http://www.takimithemes.com', 'carzine' ) ); ?>">
-                    <?php printf( __( 'Carzine ', 'carzine' )); ?></a> Theme, Powered by WordPress and sponsored by <a href="<?php echo esc_url('https://www.sdm.gr'); ?>" target="_blank"><?php echo esc_html__('SDM - Digital Marketing Agency','carzine'); ?></a>
+                    <?php printf( __( 'Carzine ', 'carzine' )); ?></a> Theme, Powered by WordPress and sponsored by <a href="<?php echo esc_url('https://www.sdm.gr'); ?>" target="_blank"><?php echo esc_html__('SDM - Digital Marketing Agency','carzine'); ?></a>. Extended and cleaned up by <a href="https://autotagebuch.net/" target="_blank">autotagebuch.net</a>.
                     </p>
                 <?php endif;?>
                 </div>
